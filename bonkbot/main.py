f1dab3eba593bb648998ff39b950ad4648912a82
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-06-19 21:51:28 +0200

    add logging and root exception handler

--- bonkbot/main.py
+++ bonkbot/main.py
@@ -1,13 +1,26 @@
+import logging
 import discord
 
 from .bot import BonkBot
 
 
 def main():
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = BonkBot(intents=intents)
-    client.run("token")
+    logging.basicConfig(
+        format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s",
+        level=logging.DEBUG,
+    )
+    logger = logging.getLogger()
+
+    try:
+
+        intents = discord.Intents.default()
+        intents.message_content = True
+        client = BonkBot(intents=intents)
+        client.run("token")
+
+    except Exception as e:
+        logger.exception("Exception occured")
+        exit(1)
 
 
 if __name__ == "__main__":
