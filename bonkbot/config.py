e030ca6be330e2128ee02ad6f65c1a9e5e0617f3
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-06-20 22:47:42 +0200

    set env prefix

--- bonkbot/config.py
+++ bonkbot/config.py
@@ -1,7 +1,9 @@
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class BotConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix='BONKBOT_')
+    
     token: str
     log_level: str = "info"
