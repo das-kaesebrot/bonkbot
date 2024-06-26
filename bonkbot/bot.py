f30e8e20e42cae06e1455e1b89166e11291df39a
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-06-27 01:53:07 +0200

    add init args, logging, typing for message

--- bonkbot/bot.py
+++ bonkbot/bot.py
@@ -1,14 +1,30 @@
+import logging
 import discord
+
+from .db.user_service import UserService
 
 
 class BonkBot(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
+    __user_service: UserService
+    __logger = logging.getLogger("bot")
 
-    async def on_message(self, message):
+    def __init__(
+        self,
+        *,
+        user_service: UserService,
+        intents: discord.Intents,
+        **options: logging.Any,
+    ) -> None:
+        self.__user_service = user_service
+        super().__init__(intents=intents, **options)
+
+    async def on_ready(self):
+        self.__logger.info(f"Logged on as {self.user}")
+
+    async def on_message(self, message: discord.Message):
         # don't respond to ourselves
         if message.author == self.user:
             return
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+        if message.content == "ping":
+            await message.channel.send("pong")
