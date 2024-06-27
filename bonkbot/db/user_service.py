53d3da19587f08c7072ec7f704bc48e4a6fb8b91
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-06-27 11:12:41 +0200

    add initial orm getters to dataservice

--- bonkbot/db/user_service.py
+++ bonkbot/db/user_service.py
@@ -1,2 +1,57 @@
-class UserService():
-    pass
+from sqlalchemy import create_engine, Engine, select
+from sqlalchemy.orm import sessionmaker
+
+from ..models.models import User, Guild
+
+
+class DataService:
+    __engine: Engine = None
+    __session = None
+
+    def __init__(self, *, connection_string: str = "sqlite://") -> None:
+        self.__engine = create_engine(connection_string, echo=True)
+        self.__session = sessionmaker(self.__engine)
+
+    def get_user(self, user_id: int) -> User:
+        """Gets a user by the specified user_id. Always returns a value, either an existing user or a new one generated on the fly.
+
+        Args:
+            user_id (int): the user id
+
+        Returns:
+            User: the user that was found or just added to the database
+        """
+        with self.__session.begin() as session:
+            select_statement = select(User).where(User.id.is_(user_id))
+
+            user = session.scalars(select_statement).one_or_none()
+
+            if user:
+                return user
+            
+            # if no user was found, generate one
+            new_user = User(id=user_id, bonks=0, guilds=[])
+            session.add(new_user)
+            return new_user
+        
+    def get_guild(self, guild_id: int) -> Guild:
+        """Gets a guild (server) by the specified guild_id. Always returns a value, either an existing guild or a new one generated on the fly.
+
+        Args:
+            guild_id (int): the guild id
+
+        Returns:
+            Guild: the guild that was found or just added to the database
+        """
+        with self.__session.begin() as session:
+            select_statement = select(Guild).where(Guild.id.is_(guild_id))
+
+            guild = session.scalars(select_statement).one_or_none()
+
+            if guild:
+                return guild
+            
+            # if no guild was found, generate one
+            new_guild = Guild(id=guild, prefix="!", users=[])
+            session.add(new_guild)
+            return new_guild
