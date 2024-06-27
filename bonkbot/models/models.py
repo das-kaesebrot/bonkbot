f4c677068244bb04c74b30b75740bab4536898f8
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-06-27 10:52:00 +0200

    add many to many relationship

--- bonkbot/models/models.py
+++ bonkbot/models/models.py
@@ -1,5 +1,5 @@
 from typing import List
-from sqlalchemy import String
+from sqlalchemy import Column, ForeignKey, String, Table
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -10,17 +10,27 @@ class Base(DeclarativeBase):
     pass
 
 
+association_table = Table(
+    "association_table",
+    Base.metadata,
+    Column("left_id", ForeignKey("left_table.id"), primary_key=True),
+    Column("right_id", ForeignKey("right_table.id"), primary_key=True),
+)
+
+
 class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     bonks: Mapped[int] = mapped_column()
     guilds: Mapped[List["Guild"]] = relationship(
-        back_populates="user", cascade="all, delete-orphan"
+        secondary=association_table, back_populates="users"
     )
+
 
 class Guild(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     prefix: Mapped[str] = mapped_column(String(1))
-    users: Mapped["User"] = relationship(back_populates="guild")
-    
+    users: Mapped[List["User"]] = relationship(
+        secondary=association_table, back_populates="guilds"
+    )
