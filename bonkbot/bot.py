b63f7526be40b4dd7ae744e8fc1f48aaf0c61dc6
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-06-27 11:45:51 +0200

    add stubs for handling bot commands

--- bonkbot/bot.py
+++ bonkbot/bot.py
@@ -2,6 +2,7 @@ import logging
 import discord
 
 from .db.data_service import DataService
+from .enums.bot_command import BotCommand
 
 
 class BonkBot(discord.Client):
@@ -22,9 +23,32 @@ class BonkBot(discord.Client):
         self.__logger.info(f"Logged on as {self.user}")
 
     async def on_message(self, message: discord.Message):
+        prefix = self.__data_service.get_guild(message.guild.id).prefix
+        
+        # get message with all whitespace around it removed
+        message_content = message.content.strip()
+        
+        # ignore all messages not starting with our prefix
+        if not message_content.startswith(prefix):
+            return
+        
+        # remove the prefix
+        message_content = message_content.removeprefix(prefix)
+        
         # don't respond to ourselves
         if message.author == self.user:
             return
-
-        if message.content == "ping":
-            await message.channel.send("pong")
+        
+        # the poor man's switch case
+        # handle different bot commands, ignoring all others that don't fit
+        if message_content.startswith(BotCommand.PREFIX):
+            # get or set prefix here
+            pass
+        elif message_content.startswith(BotCommand.BONKS):
+            # get a users bonks here
+            # message.guild.query_members()
+            pass
+        elif message_content.startswith(BotCommand.BONK):
+            # bonk a user here
+            pass
+        
