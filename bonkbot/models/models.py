b4796a14a5bf9c9c93402e1ba831418f58bb8b39
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-06-29 11:16:03 +0200

    extend user class to hold discord id, bonks and a single guild

--- bonkbot/models/models.py
+++ bonkbot/models/models.py
@@ -20,14 +20,27 @@ user_guild = Table(
 
 class User(Base):
     __tablename__ = "users"
+    # Not the actual discord id, since we have to be scoped to a guild -> generate a new value instead
+    # The discord id is scoped across all of discord
     id: Mapped[int] = mapped_column(primary_key=True)
-    bonks: Mapped[int] = mapped_column()
-    guilds: Mapped[List["Guild"]] = relationship(
-        secondary=user_guild, back_populates="users"
-    )
+    discord_id: Mapped[int] = mapped_column()
+    bonks: Mapped[List["Bonk"]] = relationship()
+    guild: Mapped["Guild"] = mapped_column(ForeignKey("guilds.id"))
+    horny_jail_until: Mapped[datetime] = mapped_column(nullable=True)
     
     def bonk(self):
-        self.bonks += 1
+        self.bonks.append(Bonk())
+        
+    def bonk_amount(self):
+        return len(self.bonks)
+    
+    def send_to_horny_jail(self, jail_time_seconds: int, jail_start: datetime = datetime.now()):
+        self.horny_jail_until = jail_start + timedelta(seconds=jail_time_seconds)
+        
+    @staticmethod
+    def get_id(discord_id: int, guild_id: int) -> int:
+        digest = hashlib.shake_256(f"{discord_id}{guild_id}").digest(32) # collision free?
+        return int.from_bytes(digest)
 
 class Guild(Base):
     __tablename__ = "guilds"
