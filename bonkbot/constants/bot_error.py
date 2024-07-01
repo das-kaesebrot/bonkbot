d218917b55fb4ce58ad73bcc59d9940151398105
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-07-01 22:08:42 +0200

    add message constants classes

---
+++ bonkbot/constants/bot_error.py
@@ -0,0 +1,4 @@
+class BotError():
+    INVALID_PREFIX = "⚠️ Invalid prefix supplied! Prefix has to be a single non-white space character. Given value: `{}`"
+    NO_USER_FOUND = "⚠️ Couldn't find any users by `{}`"
+    MISSING_USER = "⚠️ User needs to be specified!"
---
+++ bonkbot/constants/bot_message.py
@@ -0,0 +1,5 @@
+class BotMessage():
+    GUILD_PREFIX_INFO = "Guild is using prefix `{}`"
+    GUILD_PREFIX_SET = "Set guild command prefix to `{}`"
+    
+    USER_BONKS_INFO = "User **{name}** has been bonked {amount} times so far"
