afe807794dae91ba8b559047122213ed128961e1
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-07-01 22:09:16 +0200

    feature: load config from json file

--- bonkbot/config.py
+++ bonkbot/config.py
@@ -1,10 +1,27 @@
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Dict, Tuple, Type
+from pydantic_settings import BaseSettings, JsonConfigSettingsSource, PydanticBaseSettingsSource, SettingsConfigDict
 
 
 class BotConfig(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix='BONKBOT_')
+    model_config = SettingsConfigDict(env_prefix='BONKBOT_', json_file="config.json")
     
     token: str
     log_level: str = "info"
     db_connection_string: str = "sqlite://"
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            JsonConfigSettingsSource(settings_cls=settings_cls),
+            file_secret_settings,
+        )
