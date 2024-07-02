340fcbdeb5afb3fe58e6af333c8fa316d56aa8c0
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2024-07-02 15:54:25 +0200

    add error messages

--- bonkbot/constants/bot_error.py
+++ bonkbot/constants/bot_error.py
@@ -2,3 +2,5 @@ class BotError():
     INVALID_PREFIX = "⚠️ Invalid prefix supplied! Prefix has to be a single non-white space character. Given value: `{}`"
     NO_USER_FOUND = "⚠️ Couldn't find any users by `{}` or no users were mentioned!"
     MISSING_USER = "⚠️ User needs to be specified!"
+    MISSING_ROLE_MENTION = "⚠️ Exactly one role has to be mentioned!"
+    MISSING_NUMBER = "⚠️ Exactly one number has to be specified!"
