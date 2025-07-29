5d5764e4958a619eba1f16f3730628174afc3127
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-07-29 10:57:15 +0200

    feature: log retrieved/cached guild ids

--- bonkbot/bot.py
+++ bonkbot/bot.py
@@ -443,6 +443,8 @@ class BonkBot(discord.Client):
         
         stale_guild_ids = guilds_cached.difference(guilds_discord)
         
+        self.__logger.debug(f"Guilds that client is part of: {guilds_discord}")
+        self.__logger.debug(f"Guilds that are cached in database: {guilds_cached}")
         self.__logger.info(f"Removing stale guilds from database: {stale_guild_ids}")
         self.__data_service.delete_guilds(stale_guild_ids)
 
